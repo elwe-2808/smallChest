726af335c2bc04b553c509d2aa0126cec41f2f56
e191154 <e191154@student.hers.be>
2023-08-29 22:16:52 +0200

    change font tech

---
+++ front/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+	<div class="chest">
+		<div class="background">
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+		<RouterView />
+	</div>
+</template>
---
+++ front/src/views/FeatureList.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="slide-from-bottom pt-32 flex justify-center">
+		<div class="w-1/2">
+			<div class="input-icon">
+				<i class="fa-solid fa-magnifying-glass"></i>
+				<input class="w-full" />
+			</div>
+		</div>
+	</div>
+</template>
+
+<style>
+@keyframes slideFromBottom {
+	0% {
+		transform: translateY(50vh);
+		opacity: 0;
+	}
+	100% {
+		transform: translateY(0);
+		opacity: 1;
+	}
+}
+
+
+.slide-from-bottom {
+	/* This section calls the slideInFromLeft animation we defined above */
+	animation: 0.5s ease-out 0s 1 slideFromBottom;
+}
+
+</style>
